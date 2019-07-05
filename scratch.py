9b17f4aa01fc67415a207d914835fa7c05783bab
Eugene Du <eugene.du@mail.utoronto.ca>
2019-07-05 17:47:57 -0400

    changes

--- scratch.py
+++ scratch.py
@@ -1,22 +1,29 @@
 from scraggle import connectivity, grid, place_word, print_grid
 def main():
     print_grid(grid)
+    # first starting point
+    start = [1,2,3,7,8,9,13,14,15]
+    end = [8,9,14,15]
 
-    # start = [1,2,3,7,8,9,13,14,15]
-    # end = [8,9,14,15]
-    # word = 'JOKING'
+    # second starting point
+    # start = [8,9,14,15]
+    # end = [15,16,21,22]
 
-    start = [8,9,14,15]
-    end = [15,16,21,22]
-    word = 'GROTESQUELY'
+    # third starting point
+    # start = [15,16,21,22]
+    # end = [22,23,28,29]
+
+    # fourth starting point
+    # start = [22,23,28,29]
+    # end = [22,23,24,28,29,30,34,35,36]
+    word = 'SOVKHOZY'
     available_chars = 'BCDFGHJKLMNPQRSTVWYXZ'
     flag,new_grid,available_chars2 = place_word(grid,connectivity,start,end,word,available_chars)
 
-    start = [15,16,21,22]
-    end = [22,23,28,29]
-    word = 'YCLEPT'
-    print(available_chars2)
-    place_word(new_grid,connectivity,start,end,word,available_chars2)
+
+    # word = 'YCLEPT'
+    # print(available_chars2)
+    # place_word(new_grid,connectivity,start,end,word,available_chars2)
     print_grid(grid)
     return
 
